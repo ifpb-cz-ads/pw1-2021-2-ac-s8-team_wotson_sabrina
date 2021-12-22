0fdf311fc73135bd0695d763e7f5b09553f88486
wotson sula <wotson.sula@academico.ifpb.edu.br>
2021-12-22 10:25:51 -0300

    commit

---
+++ questao_06.py
@@ -0,0 +1,8 @@
+def soma(L):
+    total=0
+    x=0
+    for x in range(5):
+        total+=L[x]
+        x+=1
+    return total
+
---
+++ questao_08.py
@@ -0,0 +1,5 @@
+def func(str, L):
+    for x in L:
+        if x == str:
+            return True
+    return False
